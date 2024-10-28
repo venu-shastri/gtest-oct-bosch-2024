a57af4d664dbdbe0cbafa123b01fc889a8fe1ef3
lagisettykavitha <lagisettykavitha@gmail.com>
2024-10-28 16:58:33 +0530

    Update stringCalculator.TestFixture.cpp

--- stringCalculator.TestFixture.cpp
+++ stringCalculator.TestFixture.cpp
@@ -20,4 +20,20 @@ TEST_F(string_calculator_add,positive_cases){
     ASSERT_EQ(objUnderTest->Add("0"), 0);
     ASSERT_EQ(objUnderTest->Add("1"), 1);
     ASSERT_EQ(objUnderTest->Add("1,2"), 3);
+    ASSERT_EQ(objUnderTest->Add("//;\n1;2"), 3);
+    ASSERT_EQ(objUnderTest->Add("42,1001,3",45);
+    ASSERT_EQ(objUnderTest->Add("//[***]\n8***2***3"), 13);
+    ASSERT_EQ(objUnderTest->Add("//[*][%]\n4*2%3"), 9);
+    ASSERT_EQ(objUnderTest->Add("//[**][%^]\n4**1%^9"), 14);
   }
+
+TEST_F(string_calculator_add, ThrowsExceptionForNegativeNumbers) {
+    try {
+        objUnderTest->Add("1,-2,-4,5");
+    } catch (std::invalid_argument const &err) {
+        ASSERT_EQ(err.what(), std::string("Negatives not allowed: -2,-4"));
+    } catch (...) {
+        FAIL() << "Expected std::invalid_argument";
+    }
+}
+    
