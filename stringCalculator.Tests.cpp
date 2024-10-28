352ad0b37e28684ad9792e7cd33684ac93e777c0
lagisettykavitha <lagisettykavitha@gmail.com>
2024-10-28 14:44:15 +0530

    Update stringCalculator.Tests.cpp

--- stringCalculator.Tests.cpp
+++ stringCalculator.Tests.cpp
@@ -1,1 +1,16 @@
+#include "stringCalculator.h"
+#include <gtest/gtest.h>
 
+//TEST(TestSuiteName, TestCaseName)
+TEST(string_calculator_add,when_passed_a_single_number_returns_0_for_Empty){
+  //Arrange
+  stringCalculator objUnderTest;
+  string input="";
+  int expectedValue=0;
+
+  //Act
+  int actualValue=objUnderTest.Add(input);
+
+  //ASSERT
+  ASSERT_EQ(expectedValue,actualValue);
+  }
