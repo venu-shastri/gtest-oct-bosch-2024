32025cc767b8949c312ccb93504dff6b4531da99
lagisettykavitha <lagisettykavitha@gmail.com>
2024-10-28 16:51:59 +0530

    Update stringCalculator.TestFixture.cpp

--- stringCalculator.TestFixture.cpp
+++ stringCalculator.TestFixture.cpp
@@ -16,8 +16,8 @@ protected:
 
 //TEST(TestSuiteName, TestCaseName)
 TEST_F(string_calculator_add,positive_cases){
-    EXPECT_EQ(objUnderTest->Add(""), 0);
-    EXPECT_EQ(objUnderTest->Add("0"), 0);
-    EXPECT_EQ(objUnderTest->Add("1"), 0);
+    ASSERT_EQ(objUnderTest->Add(""), 0);
+    ASSERT_EQ(objUnderTest->Add("0"), 0);
+    ASSERT_EQ(objUnderTest->Add("1"), 1);
     ASSERT_EQ(objUnderTest->Add("1,2"), 3);
   }
