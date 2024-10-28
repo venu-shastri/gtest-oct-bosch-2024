09a74fec8ead4cdcda59b9aefe2309f90b98848f
Venugopal K <venu.shastri@gmail.com>
2024-10-28 14:42:26 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,1 +1,16 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
 
+//TEST(TestSuiteName, TestCaseName)
+TEST(string_calculator_add,when_passed_a_single_number_returns_0_for_Empty){
+  //Arrange
+  StringCalculator objUnderTest;
+  string input="";
+  int expectedValue=0;
+
+  //Act
+  objUnderTest.(input);
+  
+  
+
+}
