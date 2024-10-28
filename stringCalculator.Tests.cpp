047d432b4ef3ca049c694875e51739af5fc2e524
lagisettykavitha <lagisettykavitha@gmail.com>
2024-10-28 15:03:27 +0530

    Update stringCalculator.Tests.cpp

--- stringCalculator.Tests.cpp
+++ stringCalculator.Tests.cpp
@@ -28,3 +28,17 @@ TEST(string_calculator_add,when_passed_a_single_number_returns_0_for_0){
   //ASSERT
   ASSERT_EQ(expectedValue,actualValue);
   }
+
+//TEST(TestSuiteName, TestCaseName)
+TEST(string_calculator_add,when_passed_a_single_number_returns_1_for_1){
+  //Arrange
+  StringCalculator objUnderTest;
+  string input="1";
+  int expectedValue=1;
+
+  //Act
+  int actualValue=objUnderTest.Add(input);
+
+  //ASSERT
+  ASSERT_EQ(expectedValue,actualValue);
+  }
