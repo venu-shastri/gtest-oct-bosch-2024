4b0f9fe107d38563cd01a62a94f5f4db4cc7d311
lagisettykavitha <lagisettykavitha@gmail.com>
2024-10-28 16:47:41 +0530

    Update stringCalculator.TestFixture.cpp

--- stringCalculator.TestFixture.cpp
+++ stringCalculator.TestFixture.cpp
@@ -1,1 +1,23 @@
+#include "stringCalculator.h"
+#include <gtest/gtest.h>
 
+class string_calculator_add :  public ::testing::Test{
+protected:
+    StringCalculator *objUnderTest;
+    void SetUp() override
+    {
+      objUnderTest = new StringCalculator();
+    }
+    void TearDown() 
+    {
+      delete objUnderTest;
+    }
+};
+
+//TEST(TestSuiteName, TestCaseName)
+TEST_F(string_calculator_add,positive_cases){
+    EXPECT_EQ(objUnderTest->Add(""), 0);
+    EXPECT_EQ(objUnderTest->Add("0"), 0);
+    EXPECT_EQ(objUnderTest->Add("1"), 0);
+    ASSERT_EQ(calculator->Add("1,2"), 3);
+  }
