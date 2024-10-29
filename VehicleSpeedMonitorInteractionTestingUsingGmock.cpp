cb6524ea91ea666fd8b10ee3175d5f8b333707f4
Venugopal K <venu.shastri@gmail.com>
2024-10-29 17:30:50 +0530

    Update VehicleSpeedMonitorInteractionTestingUsingGmock.cpp

--- VehicleSpeedMonitorInteractionTestingUsingGmock.cpp
+++ VehicleSpeedMonitorInteractionTestingUsingGmock.cpp
@@ -2,6 +2,7 @@
 #include <gmock/gmock.h>
 using testing::StrEq; //Built In Matcher
 using testing::Return; //Built in Action
+using testing::_;
 
 
 class MockLogger:public ILogger{
@@ -33,8 +34,12 @@ SpeedMonitor objUnderTest{40};
 MockCommunicator communicator;
 MockSpeedSensor speedSensor;
 MockLogger logger;
-EXPECT_CALL(speedSensor,getCurrentSpeed()).WillOnce(Return(50));
-EXPECT_CALL(communicator,pushMessage(StrEq("Current Speed in Miles 31.0686"))).WillOnce(Return(300));
+ EXPECT_CALL(sensor, getCuurentSpeed()).WillOnce(Return(60));  // Speed below threshold  
+ EXPECT_CALL(communicator, pushMessage(_)).WillOnce(450);      // No alert expected
+    EXPECT_CALL(logger, write(_)).Times(2);                  // Log is expected
+
+//EXPECT_CALL(speedSensor,getCurrentSpeed()).WillOnce(Return(50));
+//EXPECT_CALL(communicator,pushMessage(StrEq("Current Speed in Miles 31.0686"))).WillOnce(Return(300));
 objUnderTest.monitor(&communicator,&speedSensor,&logger);
 }
 
