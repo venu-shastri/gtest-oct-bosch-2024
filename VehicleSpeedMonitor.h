8031f87f0f833c01b5be63c9a12f2a8947ddb285
Venugopal K <venu.shastri@gmail.com>
2024-10-29 15:35:20 +0530

    Update VehicleSpeedMonitor.h

--- VehicleSpeedMonitor.h
+++ VehicleSpeedMonitor.h
@@ -1,36 +1,19 @@
 #include <iostream>
 #include <random> 
 #include <string>
+#include "ILogger.h"
+#include "IMessageCommunicator.h"
+#include "ISpeedSensor.h"
 using namespace std;
 
-class IOTCloudCommunicator{
-    public:
-          int  pushMessage(string message){
-              //Interact with internet and push the message to MessageQueue
-            //if communiction succesfull return value from ramge {200 -400} status code otherwise returns errocode (400 - 500)
-              std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(200, 500);
-                return distr(gen);
-            
-          }
 
-};
-class BNFSpeedSensor{
-        public:
-            int   getCurrentSpeed(){
-                std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(1, 100);
-                return distr(gen);
-            }
-};
 
 class SpeedMonitor{
     private : int _speedThreshold;
   
       public:
           SpeedMonitor(int speedThreshold): _speedThreshold{speedThreshold} {}
-          void monitor();
+            //Dependency Injection 
+          void monitor(IMessageCommunicator* communicator,ISpeedSensor* sensor,ILogger* logger);
 };
 
