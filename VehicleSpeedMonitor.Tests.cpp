35ce412140a94b1a1e7dc6f95d6032ac64e4a798
Venugopal K <venu.shastri@gmail.com>
2024-10-29 16:25:43 +0530

    Update VehicleSpeedMonitor.Tests.cpp

--- VehicleSpeedMonitor.Tests.cpp
+++ VehicleSpeedMonitor.Tests.cpp
@@ -1,6 +1,9 @@
 #include "VehicleSpeedMonitor.h"
+#include <gtest/gtest.h>
 
 TEST(VehicleSpeedMonitorTestSuite,AssertValidInstanceWithSpeedThreshold){
 
-  
+    int speedThreshold = 70;  // Sample threshold
+    SpeedMonitor monitor{speedThreshold};
+     EXPECT_EQ(monitor.getSpeedThreshold(), speedThreshold);
 }
