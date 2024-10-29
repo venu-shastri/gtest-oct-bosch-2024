82dad0197468f6f7fcf2171365a9b10d32298647
Venugopal K <venu.shastri@gmail.com>
2024-10-29 16:21:04 +0530

    Update BNFSpeedSensor.cpp

--- BNFSpeedSensor.cpp
+++ BNFSpeedSensor.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-class BNFSpeedSensor{
+class BNFSpeedSensor:public ISpeedSensor{
         public:
             int   getCurrentSpeed(){
                 std::random_device rd;
