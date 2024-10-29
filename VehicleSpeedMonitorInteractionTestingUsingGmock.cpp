11b07ef50bbca311ccf5ec8b15ef42a2220e8a19
Venugopal K <venu.shastri@gmail.com>
2024-10-29 17:20:22 +0530

    Update VehicleSpeedMonitorInteractionTestingUsingGmock.cpp

--- VehicleSpeedMonitorInteractionTestingUsingGmock.cpp
+++ VehicleSpeedMonitorInteractionTestingUsingGmock.cpp
@@ -1,1 +1,38 @@
+#include "VehicleSpeedMonitor.h"
+#include <gmock/gmock.h>
+using testing::StrEq;
+
+class MockLogger:public ILogger{
+public:
+ MOCK_METHOD(void, write,(const string& message));
+    
+};
+
+class MockCommunicator:public IMessageCommunicator{
+public:
+    MOCK_METHOD(int, pushMessage,(const string& message));
+};
+
+class MockSpeedSensor:public ISpeedSensor{
+public:
+MOCK_METHOD( int ,getCurrentSpeed,())
+};
+
+TEST(VehicleSpeedMonitorTestSuite,LoggerInteractionTesting){
+SpeedMonitor objUnderTest{150};
+MockCommunicator communicator;
+MockSpeedSensor speedSensor;
+MockLogger logger;
+EXPECT_CALL(logger,write(StrEq("_speedThreshold value must be in the ramge {1-100} 150")));
+objUnderTest.monitor(&communicator,&speedSensor,&logger);
+}
+TEST(VehicleSpeedMonitorTestSuite,LoggerInteractionTesting){
+SpeedMonitor objUnderTest{40};
+MockCommunicator communicator;
+MockSpeedSensor speedSensor;
+MockLogger logger;
+EXPECT_CALL(speedSensor,getCurrentSpeed()).WillOnce(Return(50));
+EXPECT_CALL(communicator,pushMessage(StrEq(Current Speed in Miles 31.0686"))).WillOnce(Return(300));
+objUnderTest.monitor(&communicator,&speedSensor,&logger);
+}
 
