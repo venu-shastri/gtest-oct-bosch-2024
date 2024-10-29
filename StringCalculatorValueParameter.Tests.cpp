d481df525be5c4df2d41d085bc4f7119c87fd270
lagisettykavitha <lagisettykavitha@gmail.com>
2024-10-29 10:59:46 +0530

    Update StringCalculatorValueParameter.Tests.cpp

--- StringCalculatorValueParameter.Tests.cpp
+++ StringCalculatorValueParameter.Tests.cpp
@@ -1,4 +1,4 @@
-#include "StringCalculator.h"
+#include "stringCalculator.h"
 #include <gtest/gtest.h>
 #include <tuple>
 using namespace std;
@@ -22,12 +22,17 @@ class StringCalculatorAddParameterizedFixture:
 
 };
 INSTANTIATE_TEST_SUITE_P(ValidValuesDataSet,StringCalculatorAddParameterizedFixture,testing::Values(
-  make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3)
+  make_tuple("",0),make_tuple("0",0),make_tuple("1",1),make_tuple("1,2",3),make_tuple("1,2,3", 6), make_tuple("1\n2,3", 6), make_tuple("//;\n1;2", 3)
 ));
 INSTANTIATE_TEST_SUITE_P(NegetiveValuesDataSet,StringCalculatorAddParameterizedFixture,testing::Values(
-  make_tuple("",10),make_tuple("0",12),make_tuple("1",10),make_tuple("1,2",6)
+  make_tuple("",10),make_tuple("0",12),make_tuple("1",10),make_tuple("1,2",6),make_tuple("1,-2", 10), make_tuple("1,-2,-3", 6)
 ));
 
+INSTANTIATE_TEST_SUITE_P(MixedScenariosDataSet, StringCalculatorAddParameterizedFixture, testing::Values( 
+make_tuple("2,1001", 2), // ignores 1001 
+make_tuple("//[***]\n1***2***3", 6), // multiple characters delimiter 
+make_tuple("//[*][%]\n1*2%3", 6), // multiple single character delimiters 
+make_tuple("//[**][%%]\n1**2%%3", 6) // multiple multi character delimiters ));
 
 TEST_P(StringCalculatorAddParameterizedFixture, DataDrivenTestCase){
     string input=std::get<0>(GetParam());
