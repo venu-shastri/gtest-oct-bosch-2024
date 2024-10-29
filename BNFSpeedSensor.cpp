8c687bc1e591c4df7fd19c1dd6ce74f1b98242bc
Venugopal K <venu.shastri@gmail.com>
2024-10-29 15:31:05 +0530

    Create BNFSpeedSensor.cpp

---
+++ BNFSpeedSensor.cpp
@@ -0,0 +1,14 @@
+#include "ISpeedSensor.h"
+#include <random> 
+#include <string>
+using namespace std;
+
+class BNFSpeedSensor{
+        public:
+            int   getCurrentSpeed(){
+                std::random_device rd;
+                std::mt19937 gen(rd());
+                std::uniform_int_distribution<> distr(1, 100);
+                return distr(gen);
+            }
+};
