0829821781986aa4b8b0d5e87739633a4e74e7fb
lagisettykavitha <lagisettykavitha@gmail.com>
2024-10-29 10:44:36 +0530

    Update StringCalculatorDataDrivenTests.cpp

--- StringCalculatorDataDrivenTests.cpp
+++ StringCalculatorDataDrivenTests.cpp
@@ -8,27 +8,28 @@ using namespace std;
 class StringCalculatorDataDrivenAddFixture:public testing::Test{
   protected:
     StringCalculator *objUnderTest;
+    vector<tuple<string,int>> dataSet;
   void SetUp() override {
       objUnderTest=new StringCalculator();
+    dataSet.push_back(make_tuple("",0));
+    dataSet.push_back(make_tuple("0",10));
+    dataSet.push_back(make_tuple("1",1));
+    dataSet.push_back(make_tuple("1,2",3));
   }
   void TearDown(){
         delete objUnderTest;
   }
-};
-
-void assertEachDataRow(string input,int expectedValue){
+  void assertEachDataRow(string input,int expectedValue){
     int actualValue=objUnderTest->Add(input);
     ASSERT_EQ(actualValue,expectedValue);
 }
+};
+
+
 TEST_F(StringCalculatorDataDrivenAddFixture, DataDrivenTest){
-    vector<tuple<string,int>> dataSet;
-    dataSet.push_back(make_tuple("",0));
-    dataSet.push_back(make_tuple("0",0));
-    dataSet.push_back(make_tuple("1",1));
-    dataSet.push_back(make_tuple("1,2",3));
     //iterate
   for (tuple<string,int> dataRow : dataSet){
-        assertEachDataRow(dataRow.get<string>(0),dataRow.get<int>(1));
+        assertEachDataRow(std::get<0>(dataRow),std::get<1>(dataRow));
     }
   
 }
