3439cb9d3a667f86a5586a88e9b17054774d6b41
Venugopal K <venu.shastri@gmail.com>
2024-10-29 11:27:03 +0530

    Create VehicleSpeedMonitor.cpp

---
+++ VehicleSpeedMonitor.cpp
@@ -0,0 +1,23 @@
+#include "VehicleSpeedMonitor.h"
+void SpeedMonitpr::monitor(){
+            if(_speedThreshold  < 1 || _speedThreshold > 100){
+              cout<<"_speedThreshold value must be in the ramge {1-100} "<<_speedThreshold<<endl;
+            }
+              
+            BNFSpeedSensor speedSensorInstance;
+            IOTCloudCommunicator cloudCommuniccator;
+            int currentSpeedInKmph=speedSensorInstance.getCurrentSpeed();
+            
+            cout<<"Current Speed In Kmph "<<currentSpeedInKmph<<endl;
+            if(currentSpeedInKmph > _speedThreshold){
+                  double mph = currentSpeedInKmph * 0.621371;
+                  string message="Current Speed in Miles "+ to_string(mph);
+                 int statusCode= cloudCommuniccator.pushMessage(message);
+                  if(statusCode > 400){
+                      //Log Message to Console
+                       cout<<"Error In Communication Unable to Contact Server "<<message<< endl;
+                  }
+                 
+            }
+              
+          }
